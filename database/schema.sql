988906304f10aba5849122e7ec44829ed69d8c9d
Berkley Sitterson <BerkleySitterson@gmail.com>
2023-07-30 20:00:59 -0400

    Adding testing directory and database files

---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    username VARCHAR(255) PRIMARY KEY NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+)
